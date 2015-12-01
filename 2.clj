337779ec21925c6787ae886d1bc0b06b8d99900c
KevinBacas <bacas.kevin@hotmail.fr>
2015-12-01 09:13:57 +0100

    Challenge 2 comments

--- 2.clj
+++ 2.clj
@@ -1,7 +1,10 @@
+; Fibonaci definition
 (def fib (cons 1 (cons 1 (lazy-seq (map + fib (rest fib))))))
 
+; Return true if number is under 4m
 (defn isUnder4Million
   [x]
   (< x 4000000))
 
+; Calculating sum for of even fibonacci number under 4m
 (reduce + (filter even? (filter isUnder4Million (take 50 fib))))
